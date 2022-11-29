fc82d1ac8840a9d4ff24274b40dc73377d3a354d
Daniel171202 <daniel.aldazosa@gmail.com>
2022-11-28 22:26:51 -0400

    version 10 de la noche

--- app/entrada.py
+++ app/entrada.py
@@ -11,11 +11,13 @@ entrada = Blueprint('entrada', __name__, template_folder='app/templates')
 def Index():
     print("ODIOAKI")
     #return render_template('pruebaLogin/Login.html')
-    return render_template('quitar.html')
+    #return render_template('Registro/Registro.html')
+    return render_template('combi.html')
     #return render_template('PaginaPrincipal/Principal.html')
 
 @entrada.route('/')
 def Registro_Frame():
+    
     return render_template('Registro/Registro.html')
 
 
@@ -23,12 +25,63 @@ def Registro_Frame():
 def Login_Frame():
     return render_template('pruebaLogin/Login.html')
 
+@entrada.route('/')
+def Quitar_Frame():
+    return render_template('quitar.html')
 
 
 
-#salkdejals
 
 
+#para ir a menu de Combi
+@entrada.route('/templates/combi.html')
+def Combi():
+    return render_template('combi.html')
+
+#para ir a menu de Salchipapus
+@entrada.route('/templates/salchipapus.html')
+def Salchipapus():
+    return render_template('salchipapus.html')
+
+#para ir a menu de maito
+@entrada.route('/templates/maito.html')
+def maito():
+    return render_template('maito.html')
+
+#para ir a menu de pocho
+@entrada.route('/templates/pocho.html')
+def pocho():
+    return render_template('pocho.html')
+
+#para ir a "Inicio"
+@entrada.route('/templates/index.html')
+def irInicio():
+    return render_template('index.html')
+
+#para ir a  Registro
+#############################
+@entrada.route('/templates/Registro/Registro.html', methods=['GET', 'POST'])
+def Registro():
+    return render_template('/Registro/Registro.html')
+
+
+#para volver a ATRAS desde Registro
+#############################
+@entrada.route('/templates/pruebaLogin/Login.html', methods=['GET', 'POST'])
+def VolverAtras():
+    return render_template('/pruebaLogin/Login.html')
+
+
+
+
+
+
+
+#agregar productos
+@entrada.route('/templates/carrito.html')
+def Agregar_CombiBBQ():
+    Agregar_carrito("CombiBBQ",33)
+    return render_template('combi.html')
 
 
 
@@ -40,7 +93,7 @@ def Iniciar_Sesion():
         usuario = request.form['user']
         contra = request.form['pass']
         cur = mysql.connection.cursor()        
-        cur.execute('Select count(nombreUsuario) from datosUsuario where nombreUsuario = %s group by nombreUsuario ', [usuario])
+        cur.execute('Select count(nombreUsuario) from datosUsuario where nombreUsuario = %s and estado=1 group by nombreUsuario ', [usuario])
         data = cur.fetchone()
         cur.close()
         if not data:
@@ -52,7 +105,13 @@ def Iniciar_Sesion():
             data = cur.fetchone()
             if contra==data[0]:
                 print("accesso")
-                return render_template('PaginaPrincipal/Principal.html', entrada=data)
+                cur.execute('SELECT tipocuenta.idTipoCuenta  FROM datosusuario INNER JOIN usuario ON datosusuario.usuario_idUsuario = usuario.idUsuario INNER JOIN tipocuenta ON usuario.tipoCuenta_idTipoCuenta = tipocuenta.idTipoCuenta  WHERE datosusuario.nombreUsuario = %s ', [usuario])
+                data = cur.fetchone()
+                cur.close()
+                if data[0]==1:
+                    return render_template('index.html', entrada=data)
+                elif data[0]==3:
+                    return render_template('admin.html', entrada=data)
             else:
                 try:
                     return redirect(url_for('entrada.Login_Frame'))
@@ -66,14 +125,44 @@ def ultimoreg(tabla):
     cur.execute(cadena)
     data = cur.fetchall()
     cur.close()
-    for  row in data:
-        i=0
-        for columna in row:
-            if i==0:
-                aux=columna
-            i=i+1
-    return(aux)         
+    if not data:
+        return(0)
+    else:
+        for  row in data:
+            i=0
+            for columna in row:
+                if i==0:
+                    aux=columna
+                i=i+1
+        return(aux)         
 
+
+
+def Agregar_carrito(prod,precio):
+        print("carrito")
+        id=ultimoreg("carrito")+1
+        print (id)
+        cur = mysql.connection.cursor()        
+        cur.execute('SELECT sum(carrito.cantidad)  FROM carrito  WHERE carrito.Producto = %s group by carrito.cantidad>0', [prod])
+        data = cur.fetchone()
+        cur.close()
+        
+        if not data:
+            cur = mysql.connection.cursor()        
+            cur.execute('INSERT INTO carrito (Id_carrito, Producto, Cantidad, Precio) VALUES (%s, %s, %s, %s); ', (id,prod,1,precio))
+            flash('Register Successfully')
+            mysql.connection.commit()
+            cur.close()
+        else:
+            aux=data[0]
+            cur = mysql.connection.cursor()        
+            cur.execute(' update carrito set carrito.cantidad= %s where carrito.producto= %s ', ((aux+1,prod)))
+            flash('Carrito Updated Successfully')
+            mysql.connection.commit()
+            cur.close()
+
+
+        
 
 
 @entrada.route('/Registro/Registro.html', methods=['GET', 'POST'])
@@ -81,6 +170,7 @@ def Registro_Usuario():
     if request.method == 'POST':
         print("Registro")
         id=ultimoreg("usuario")+1
+        print (id)
         usuario = request.form['user']
         contra = request.form['pass1']
         contra2 = request.form['pass2']
@@ -89,7 +179,7 @@ def Registro_Usuario():
         fecha=now.date()
         
         cur = mysql.connection.cursor()        
-        cur.execute('Select count(nombreUsuario) from datosUsuario where nombreUsuario = %s group by nombreUsuario ', [usuario])
+        cur.execute('Select count(nombreUsuario) from datosUsuario where nombreUsuario = %s and estado = 1 group by nombreUsuario ', [usuario])
         data = cur.fetchone()
         cur.close()
         if not data:
@@ -105,7 +195,7 @@ def Registro_Usuario():
                 flash('Register Successfully')
                 mysql.connection.commit()
                 cur.close()
-                return render_template('PaginaPrincipal/Principal.html')
+                return render_template('index.html')
             else:
               print("Contraseñas no coinciden") 
               return redirect(url_for('entrada.Registro_Frame'))
@@ -115,47 +205,37 @@ def Registro_Usuario():
 
 
 
-        
-        
-       
- 
-    
-
-@entrada.route('/edit/<id>', methods=['POST', 'GET'])
-def get_contact(id):
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM entrada WHERE id = %s', (id))
-    data = cur.fetchall()
-    cur.close()
-    print(data[0])
-    return render_template('edit-contact.html', contact=data[0])
 
 
-@entrada.route('/update/<id>', methods=['POST'])
-def update_contact(id):
+
+
+@entrada.route('/Paginaweb/quitar.html', methods=['GET', 'POST'])
+def Quitar_Admin():
     if request.method == 'POST':
-        fullname = request.form['fullname']
-        phone = request.form['phone']
-        email = request.form['email']
-        cur = mysql.connection.cursor()
-        cur.execute("""
-            UPDATE entrada
-            SET fullname = %s,
-                email = %s,
-                phone = %s
-            WHERE id = %s
-        """, (fullname, email, phone, id))
-        flash('Contact Updated Successfully')
-        mysql.connection.commit()
-        return redirect(url_for('entrada.Index'))
+        print("quitar admin")
+        
+        usuario = request.form['user']
+        
+     
+        print(usuario)
+        cur = mysql.connection.cursor()        
+        cur.execute('SELECT  datosusuario.nombreUsuario  FROM datosusuario INNER JOIN usuario ON datosusuario.usuario_idUsuario = usuario.idUsuario  WHERE usuario.tipoCuenta_idTipoCuenta = 3  AND datosusuario.nombreUsuario = %s ', [usuario])
+        data = cur.fetchone()
+        cur.close()
+        if not data:
+            print("Este usuario administrador no existe")
+            return redirect(url_for('entrada.Quitar_Frame'))
+        else:
+            if data[0]==usuario:
+                cur = mysql.connection.cursor()        
+                cur.execute(' update datosusuario set datosusuario.estado=0 where datosusuario.nombreUsuario= %s ', [usuario])
+                flash('Admin Updated Successfully')
+                mysql.connection.commit()
+                cur.close()
+                
+                print("Usuario Eliminado")
+                return render_template('PaginaPrincipal/Principal.html', entrada=data)
+                #return redirect(url_for('entrada.Quitar_Frame'))
 
-
-@entrada.route('/delete/<string:id>', methods=['POST', 'GET'])
-def delete_contact(id):
-    cur = mysql.connection.cursor()
-    cur.execute('DELETE FROM entrada WHERE id = {0}'.format(id))
-    mysql.connection.commit()
-    flash('Contact Removed Successfully')
-    return redirect(url_for('entrada.Index'))
 
     
